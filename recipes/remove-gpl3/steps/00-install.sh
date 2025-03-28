b4c5c0780bce9096f1bec7eebe0bc5b1caaff7dd
zhongys-c8y <yishu.zhong@cumulocity.com>
2025-03-28 13:39:45 +0100

    Change zsh installation

--- recipes/remove-gpl3/steps/00-install.sh
+++ recipes/remove-gpl3/steps/00-install.sh
@@ -10,3 +10,20 @@ if [ -f "$RUGIX_PROJECT_DIR/.env" ]; then
 fi
 
 apk del bash util-linux wget dosfstools
+
+apk add zsh
+
+if [ -e /bin/bash ] || [ -L /bin/bash ]; then
+    echo "Remove existing /bin/bash link"
+    rm -f /bin/bash
+fi
+
+echo "Create /bin/bash -> /bin/zsh link"
+ln -s /bin/zsh /bin/bash
+
+if [ "$(readlink -f /bin/bash)" = "/bin/zsh" ]; then
+    echo "Link created"
+else
+    echo "Failed to create /bin/bash -> /bin/zsh link"
+    exit 1
+fi
